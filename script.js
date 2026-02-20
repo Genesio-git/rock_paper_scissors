8bfdbf53fbaf03f00d5f05b16a06363dfd17e627
Genesio-git <genesioftj04@gmail.com>
2026-02-20 00:03:34 -0300

    feat: implementa lógica base do jogo pedra, papel e tesoura

---
+++ script.js
@@ -0,0 +1,91 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getRandom(){
+    return Math.random();
+}
+
+function getComputerChoice(){
+    const r = getRandom();
+
+    console.log(r);
+
+    if(r < 0.33){
+        return "rock";
+    }
+    else if(r >= 0.33 && r < 0.66){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let choice = prompt("Escolha entre pedra, papel ou tesoura");
+
+    console.log(choice)
+
+    if(choice == "pedra"){
+        return "pedra";
+    }
+    else if(choice == "papel"){
+        return "papel";
+    }
+    else{
+        return "tesoura";
+    }
+
+}
+
+console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase()
+
+    if(humanChoice == "pedra" && computerChoice == "papel"){
+        console.log("Você perdeu, papel ganha de pedra!")
+        computerScore++;
+        return "Você perdeu, papel ganha de pedra!";
+    }
+    else if(humanChoice == "pedra" && computerChoice == "tesoura"){
+        console.log("Voce ganhou, pedra ganha de tesoura!")
+        humanScore++;
+        return "Voce ganhou, pedra ganha de tesoura!";
+    }
+    else if(humanChoice == "papel" && computerChoice == "pedra"){
+        console.log("Voce ganhou, papel ganha de pedra!")
+        humanScore++;
+        return "Voce ganhou, papel ganha de pedra!";
+    }
+    else if(humanChoice == "papel" && computerChoice == "tesoura"){
+        console.log("Voce perdeu, tesoura ganha de papel!")
+        computerScore++;
+        return "Voce perdeu, tesoura ganha de papel!";
+    }
+    else if(humanChoice == "tesoura" && computerChoice == "papel"){
+        console.log("Voce ganhou, tesoura ganha de papel!")
+        humanScore++;
+        return "Voce ganhou, tesoura ganha de papel!";
+    }
+    else if(humanChoice == "tesoura" && computerChoice == "pedra"){
+        console.log("Voce perdeu, pedra ganha de tesoura")
+        computerScore++;
+        return "Voce perdeu, pedra ganha de tesoura";
+    }
+    else{
+        console.log("Empate")
+        return "Empate"
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+
+const computerSelection = getComputerChoice();
+
+function playGame(){
+    
+}
