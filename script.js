36f44a1c6199ec0b70756c8ed4b6f8a727696c85
Genesio-git <genesioftj04@gmail.com>
2026-02-21 00:31:53 -0300

    feat: implementa playGame com loop de 5 rodadas

--- script.js
+++ script.js
@@ -5,25 +5,19 @@ function getRandom(){
 function getComputerChoice(){
     const r = getRandom();
 
-    console.log(r);
-
     if(r < 0.33){
-        return "rock";
+        return "pedra";
     }
     else if(r >= 0.33 && r < 0.66){
-        return "paper";
+        return "papel";
     }
     else{
-        return "scissors";
+        return "tesoura";
     }
 }
 
-console.log(getComputerChoice())
-
 function getHumanChoice(){
     let choice = prompt("Escolha entre pedra, papel ou tesoura");
-
-    console.log(choice)
 
     if(choice == "pedra"){
         return "pedra";
@@ -37,56 +31,58 @@ function getHumanChoice(){
 
 }
 
-console.log(getHumanChoice())
-
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
 
     function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase()
+        humanChoice = humanChoice.toLowerCase()
 
-    if(humanChoice == "pedra" && computerChoice == "papel"){
-        console.log("Você perdeu, papel ganha de pedra!")
-        computerScore++;
-        return "Você perdeu, papel ganha de pedra!";
-    }
-    else if(humanChoice == "pedra" && computerChoice == "tesoura"){
-        console.log("Voce ganhou, pedra ganha de tesoura!")
-        humanScore++;
-        return "Voce ganhou, pedra ganha de tesoura!";
-    }
-    else if(humanChoice == "papel" && computerChoice == "pedra"){
-        console.log("Voce ganhou, papel ganha de pedra!")
-        humanScore++;
-        return "Voce ganhou, papel ganha de pedra!";
-    }
-    else if(humanChoice == "papel" && computerChoice == "tesoura"){
-        console.log("Voce perdeu, tesoura ganha de papel!")
-        computerScore++;
-        return "Voce perdeu, tesoura ganha de papel!";
-    }
-    else if(humanChoice == "tesoura" && computerChoice == "papel"){
-        console.log("Voce ganhou, tesoura ganha de papel!")
-        humanScore++;
-        return "Voce ganhou, tesoura ganha de papel!";
-    }
-    else if(humanChoice == "tesoura" && computerChoice == "pedra"){
-        console.log("Voce perdeu, pedra ganha de tesoura")
-        computerScore++;
-        return "Voce perdeu, pedra ganha de tesoura";
-    }
-    else{
-        console.log("Empate")
-        return "Empate"
+        if(humanChoice == "pedra" && computerChoice == "papel"){
+            console.log("Você perdeu, papel ganha de pedra!")
+            computerScore++;
+            return "Você perdeu, papel ganha de pedra!";
+        }
+        else if(humanChoice == "pedra" && computerChoice == "tesoura"){
+            console.log("Voce ganhou, pedra ganha de tesoura!")
+            humanScore++;
+            return "Voce ganhou, pedra ganha de tesoura!";
+        }
+        else if(humanChoice == "papel" && computerChoice == "pedra"){
+            console.log("Voce ganhou, papel ganha de pedra!")
+            humanScore++;
+            return "Voce ganhou, papel ganha de pedra!";
+        }
+        else if(humanChoice == "papel" && computerChoice == "tesoura"){
+            console.log("Voce perdeu, tesoura ganha de papel!")
+            computerScore++;
+            return "Voce perdeu, tesoura ganha de papel!";
+        }
+        else if(humanChoice == "tesoura" && computerChoice == "papel"){
+            console.log("Voce ganhou, tesoura ganha de papel!")
+            humanScore++;
+            return "Voce ganhou, tesoura ganha de papel!";
+        }
+        else if(humanChoice == "tesoura" && computerChoice == "pedra"){
+            console.log("Voce perdeu, pedra ganha de tesoura")
+            computerScore++;
+            return "Voce perdeu, pedra ganha de tesoura";
+        }
+        else{
+            console.log("Empate")
+            return "Empate"
+        }
+
     }
 
+    for(i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(humanSelection, computerSelection));
+        console.log(computerScore);
+        console.log(humanScore)
+        }
 }
 
-    playRound(getHumanChoice, getComputerChoice)
-
-}
-
-const humanSelection = getHumanChoice();
-
-const computerSelection = getComputerChoice();
+playGame()
